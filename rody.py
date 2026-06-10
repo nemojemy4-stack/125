e0c409fddb612dae145d67e26ec811a3ffa22d0c
nemojemy4-stack <nemojemy4@gmail.com>
2026-06-11 00:28:40 +0300

    Add files via upload

---
+++ rody.py
@@ -0,0 +1,9 @@
+temperature = 24
+if ( temperature  >= 30):
+    print("It is a hot day. Stay hydrated")
+elif(20 <= temperature <= 29):
+     print("It is a warm day . Enjoy the weather")
+elif(10 <= temperature <= 19):
+    print("It is a cool day. Wear a jacket")
+else :
+    print("It is a cold day . Stay warm")
